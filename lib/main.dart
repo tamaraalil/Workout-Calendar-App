adce191d3df4f624acc3926c337e7e0ec05e8329
Tamara Alilovic <tamaraalilovic@Tamaras-MacBook-Air-2.local>
2023-03-09 19:16:16 -0500

    Added navigation to pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// Workout Calendar App
+// Group 10
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,21 +22,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Workout {
+  Workout.items({this.date = "", this.title = "", this.sets = const[], this.reps = 0, 
+  this.weight = 0, this.addToPreset = false, this.notes = ""});
+  final String date;
+  final String title;
+  final List sets;
+  final int reps;
+  final int weight;
+  final bool addToPreset;
+  final String notes;
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  bool initialStatus=false;
+  List<Workout> workoutList = [
+    Workout.items(
+      title: 'pushups',
+      date: 'March 9th, 2023',
+      sets:  [],
+      reps: 0,
+      weight: 0,
+      addToPreset: false,
+      notes: "gabagool",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -42,24 +63,123 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      drawer: Drawer(
+        child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            child: Text('\nWorkout\nCalendar', textAlign: TextAlign.center,
+              style: TextStyle (fontWeight: FontWeight.w700, fontSize: 30.0, color: Colors.white),),
+          ),
+          ListTile(
+            leading: Icon(Icons.calendar_month, color: Colors.black, size: 20.0,),
+            title: const Text('Calendar'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.add_circle_outline, color: Colors.black, size: 20.0,),
+            title: const Text('Add Preset Workout'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const PresetWorkout()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.assessment_outlined, color: Colors.black, size: 20.0,),
+            title: const Text('Analytics'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Analytics()),
+              );
+            },
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+          //gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
+          itemCount: workoutList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PresetWorkout()),
+                );
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Stack(
+                  alignment: FractionalOffset.bottomCenter,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xff1b1b1c), //#0xff7c94b6
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    Container( // workout name
+                      height: 90.0,
+                      width: 20.0,
+                      //padding: const EdgeInsets.all(10.0),
+                      alignment: Alignment.center,
+                      child: Row (
+                        children: <Widget> [
+                          Text(workoutList[index].title, 
+                            style: TextStyle (fontWeight: FontWeight.w700, fontSize: 20.0, color: Colors.black),),
+                        ],
+                      ),
+                    ),
+                 ],
+                ),
+              ),
+            );
+          },),      
+    );
+  }
+}
+
+
+// -------------------- ADD PRESET WORKOUT PAGE ---------------------
+
+class PresetWorkout extends StatelessWidget {
+  const PresetWorkout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Preset Workout'),
+      ),
+      body: Center(
+        child: Text('Add Preset Workout', 
+          style: TextStyle (fontSize: 15.0, color: Colors.black),),
+      ),
+    );
+  }
+}
+
+// -------------------- ANALYTICS PAGE ---------------------
+
+class Analytics extends StatelessWidget {
+  const Analytics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Analytics'),
+      ),
+      body: Center(
+        child: Text('Analytics', 
+          style: TextStyle (fontSize: 15.0, color: Colors.black),),
       ),
     );
   }
